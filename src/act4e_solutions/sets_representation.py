18f0e21cbd37e13ddd240d2e2ed3861b2511aa56
Davis Ismoilov <fismoilov20@gmail.com>
2024-08-28 13:33:50 +0500

    ex1

--- src/act4e_solutions/sets_representation.py
+++ src/act4e_solutions/sets_representation.py
@@ -1,11 +1,42 @@
-from typing import Any
+from typing import cast, Any, Collection, Iterator, List, TypeVar
 
 import act4e_interfaces as I
+
+E = TypeVar("E")
+class MyFiniteSet(I.FiniteSet[E]):
+    _elements: List[E]
+    def __init__(self, elements: Collection[E]):
+        self._elements = list(elements)
+    def size(self) -> int:
+        return len(self._elements)
+    def contains(self, x: E) -> bool:
+        for y in self._elements:
+            if self.equal(x, y):
+                return True
+        return False
+    def elements(self) -> Iterator[E]:
+        return iter(self._elements)
+        # for _ in self._elements:
+        #     yield _
+
+    def save(self, h: I.IOHelper, x: E) -> I.ConcreteRepr:
+        return cast(I.ConcreteRepr, x)
+
+    def load(self, h: I.IOHelper, o: I.ConcreteRepr) -> E:
+        return cast(E, o)
 
 
 class SolFiniteSetRepresentation(I.FiniteSetRepresentation):
     def load(self, h: I.IOHelper, data: I.FiniteSet_desc) -> I.FiniteSet[Any]:
-        raise NotImplementedError()
+        if not isinstance(data, dict):
+            raise I.InvalidFormat()
+        # note: later on the format will be extended
+        if not "elements" in data:
+            raise I.InvalidFormat()
+        if not isinstance(data["elements"], list):
+            raise I.InvalidFormat()
+        return MyFiniteSet(data["elements"])
 
     def save(self, h: I.IOHelper, f: I.FiniteSet[Any]) -> I.FiniteSet_desc:
-        raise NotImplementedError()
+        all_elements = [f.save(h, _) for _ in f.elements()]
+        return {"elements": all_elements}
