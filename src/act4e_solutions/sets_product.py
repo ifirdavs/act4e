82b426c26b6e88a21a572b420f4af2fb0d9c6568
Firdavsbek Ismoilov <fismoilov20@gmail.com>
2024-09-17 22:21:53 +0500

    B.4 completed

--- src/act4e_solutions/sets_product.py
+++ src/act4e_solutions/sets_product.py
@@ -1,21 +1,49 @@
-from typing import Any, Collection, Sequence, TypeVar
-import itertools
+from typing import Any, Collection, Sequence, List, TypeVar, Iterator, cast
 
 import act4e_interfaces as I
 
-C = TypeVar("C")
-E = TypeVar("E")    # E = List[C]
+C = TypeVar("C")    # A, B, C sets
+E = TypeVar("E")    # Element of the Product: E = List[C]
 
 # Interfaces are ALL in act4e_interfaces/set_product.py
 
 # Implementations
-from .sets_representation import MyFiniteSet
-class MyFiniteMakeSetProduct(I.FiniteMakeSetProduct, MyFiniteSet):
+class MyFiniteSetProduct(I.FiniteSetProduct):
     _components: Sequence[I.FiniteSet[C]]
+    _elements: List[E]
 
-    def __init__(self, elements: Collection[I.FiniteSet[C]]):
-        self._components = list(elements)
+    def __init__(self, components: Collection[I.FiniteSet[C]]):
+        self._components = list(components)
 
+        result = [[]]
+        for comp in self._components:
+            temp_result = []
+
+            for item in result:
+                for elem in comp.elements():
+                    temp_result.append(item + [elem])
+            result = temp_result
+        
+        self._elements = result
+
+    # FiniteSet
+    def size(self) -> int:
+        return len(self._elements)
+    def elements(self) -> Iterator[E]:
+        for _ in self._elements:
+            yield _
+    
+    # Setoid
+    def contains(self, x: E) -> bool:
+        for y in self._elements:
+            if self.equal(x, y):
+                return True
+        return False
+    def save(self, h: I.IOHelper, x: E) -> I.ConcreteRepr:
+        return cast(I.ConcreteRepr, x)
+    def load(self, h: I.IOHelper, o: I.ConcreteRepr) -> E:
+        return cast(E, o)
+    
     # SetProduct
     def components(self) -> Sequence[I.FiniteSet[C]]:
         return self._components
@@ -27,4 +55,4 @@ class MyFiniteMakeSetProduct(I.FiniteMakeSetProduct, MyFiniteSet):
 
 class SolFiniteMakeSetProduct(I.FiniteMakeSetProduct):
     def product(self, components: Sequence[I.FiniteSet[C]]) -> I.FiniteSetProduct[C, Any]:
-        return MyFiniteMakeSetProduct(itertools.product(*components))
+        return MyFiniteSetProduct(components)
